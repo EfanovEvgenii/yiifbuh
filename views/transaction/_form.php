67729eae7f4144ca9e25fb58e02ac01ae6804295
Efanov Evgenii <jeezic@yandex.ru>
2017-01-05 03:10:24 +0700

    в форму операции добавлены списки выбора из справочников

--- views/transaction/_form.php
+++ views/transaction/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
 
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Transaction */
 /* @var $form yii\widgets\ActiveForm */
